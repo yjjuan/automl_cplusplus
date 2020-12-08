40dec1810682df705a159cc4c13e1aaffa0b2d6b
yjjuan <ngs816@gmail.com>
2020-12-08 00:09:10 -0800

    add more narratives in nb

---
+++ run_test.sh
@@ -0,0 +1,4 @@
+# Run the peak detecctor on the first test set.
+
+cd limbo/exp/myExp/build
+./main_test ../found_params.txt ../../../../test_set1.csv
---
+++ run_test2.sh
@@ -0,0 +1,4 @@
+# Run the peak detecctor on the second test set.
+
+cd limbo/exp/myExp/build
+./main_test ../found_params.txt ../../../../test_set2.csv
